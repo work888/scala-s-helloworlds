bb656ba0031df2c38feaa275326030cb1d9542c1
work888 <work888@i.ua>
2016-07-01 16:39:50 +0400

    create

---
+++ src/com/work888/helloworlds/Helloword.scala
@@ -0,0 +1,10 @@
+package com.work888.helloworlds
+
+/**
+  * Created by work888 on 01.07.16.
+  */
+object Helloworld {
+  def main(args: Array[String]) {
+    println("Welcome to https://github.com/work888")
+  }
+}
